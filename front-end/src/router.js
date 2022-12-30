7450103831a4e16683b414e808d7e067044cd5c6
Leno <bartlomiej.sikora@op.pl>
2022-12-30 19:39:48 +0100

    Add navigation guards

--- front-end/src/router.js
+++ front-end/src/router.js
@@ -5,7 +5,7 @@ import UserRegistration from "./pages/auth/UserRegistration.vue";
 import UsersList from "./pages/users/UsersList.vue";
 import UserEdit from "./pages/users/UserEdit.vue";
 import NotFound from "./pages/NotFound.vue";
-//import store from "./store/index.js";
+import store from "./store/index.js";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -22,13 +22,23 @@ const router = createRouter({
     {
       path: "/users",
       component: UsersList,
+      meta: { requiresAuth: true },
     },
     {
       path: "/users/edit-account",
       component: UserEdit,
+      meta: { requiresAuth: true },
     },
     { path: "/:notFound(.*)", component: NotFound },
   ],
+});
+
+router.beforeEach(function (to, _, next) {
+  if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+    next("/login");
+  } else {
+    next();
+  }
 });
 
 export default router;
